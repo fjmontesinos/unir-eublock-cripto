7ac6a93203716ff078458056bf99b8f35e9587bc
Javier Montesinos <fj.montesinos@gmail.com>
2019-11-26 06:35:49 +0100

    Added. ejecicio 1 práctica con SHA256

---
+++ ejercicio-1/hash-sha256.py
@@ -0,0 +1,17 @@
+from hashes_unir import HashType, CryptoUNIR
+
+crypto = CryptoUNIR()
+
+mensaje1 = 'Estamos aplicando de forma practica los conocimientos de criptografia'
+mensaje1Hash = crypto.calcularHash(HashType.SHA256, mensaje1, True)
+
+mensaje2 = 'criptografia'
+mensaje2Hash = crypto.calcularHash(HashType.SHA256, mensaje2, True)
+
+if( len(mensaje1Hash) == len(mensaje2Hash) ):
+    print("El hash SHA256 de los mensajes 1 y 2 son iguales en tamaño")
+else:
+    print("El hash SHA256 de los mensajes 1 y 2 NO son iguales en tamaño")
+
+mensaje3 = 'criptografio'
+mensaje3Hash = crypto.calcularHash(HashType.SHA256, mensaje3, True)
